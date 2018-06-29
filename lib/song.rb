c3e4917a29869136c7456dfbbb2f82db310913a1
tahseen447 Siddiqui <tahseenfatima1986@gmail.com>
2018-06-29 19:05:13 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,5 +1,10 @@
 class Song
 
-  attr_accessor :name, :artist, :genre
+  attr_reader :name, :artist, :genre
 
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+  end
 end
